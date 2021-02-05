53c05beddf291aa40d02c7c704b4bac4bd30604e
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2021-02-05 15:23:24 -0300

    setting up workspace and creating navbar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+   <Router>
+     <Navbar />
+   {/* <Switch>
+     
+   </Switch> */}
+   </Router>
   );
 }
 
