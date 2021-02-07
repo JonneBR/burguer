9509c53a0ca942df4f5b0f31b1657b28a84d6834
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2021-02-07 15:51:22 -0300

    landingpage component basic css style

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
+import LandingPage from './components/LandingPage/LandingPage';
 
 function App() {
   return (
    <Router>
      <Navbar />
-   {/* <Switch>
-     
-   </Switch> */}
+   <Switch>
+     <LandingPage/>
+   </Switch>
    </Router>
   );
 }
---
+++ src/components/LandingPage/LandingPage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './LandingPage.css'
+
+function LandingPage() {
+    return (
+        <>
+           <section className="landingpage-section">
+            
+            </section> 
+            <footer className="footer-content">
+                 
+            </footer>
+        </>
+    )
+}
+
+export default LandingPage
