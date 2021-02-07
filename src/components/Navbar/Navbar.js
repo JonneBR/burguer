ebc9ade13362d10d67d54c4e161b7d031d5ea12c
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2021-02-07 10:41:14 -0300

    handleClickMenu function created

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,13 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Link} from 'react-router-dom'
 import logo from '../../images/burguer-logo.png'
 import './Navbar.css'
 
 function Navbar() {
+
+    const [click, setClick] = useState(false)
+
+    const handleClickMenu = () => setClick(!click)
     return (
         <>
             <div className="navbar-container">
                 <nav className="navbar-wrapper">
+                    <div onClick={handleClickMenu} className="menu-icon">
+                    {click ? <i className="fas fa-times"></i> : <i className="fas fa-bars"></i>}
+                    </div>
                     <ul className="nav-menu">
                         <li className="nav-item">
                             <Link to='/' className='nav-link'>
