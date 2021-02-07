976fd7720bec6a89c738f05c25d6bd4713b14c83
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2021-02-07 18:44:24 -0300

    positioning items on bottom landind page

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,17 +1,27 @@
-import React from 'react'
-import './LandingPage.css'
+import React from 'react';
+import './LandingPage.css';
 
 function LandingPage() {
-    return (
-        <>
-           <section className="landingpage-section">
-            
-            </section> 
-            <footer className="footer-content">
-                 
-            </footer>
-        </>
-    )
+  return (
+    <>
+      <section className='landingpage-section'></section>
+      <div className='bottom-content'>
+        <div className='info-container'>
+          <h1 className='header-title'>Horários:</h1>
+          <p className='description'>De quarta a segunda apartir das 19h</p>
+        </div>
+        <div className='info-container'>
+          <h1 className='header-title'>Horários:</h1>
+          <p className='description'>De quarta a segunda apartir das 19h</p>
+        </div>
+        <div className='info-container'>
+          <h1 className='header-title'>Telefone:</h1>
+          <p className='description'>11998324414
+11994144238</p>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default LandingPage
+export default LandingPage;
