2993303ca2048501e408a77a03a9b4e9923451be
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2021-02-08 17:07:46 -0300

    content description css style

--- src/components/ApresentationPage/ApresentationPage.js
+++ src/components/ApresentationPage/ApresentationPage.js
@@ -13,11 +13,35 @@ function ApresentationPage() {
             <div className='image-item'>
               <img src={imgOne} alt='' />
             </div>
+
             <div className='image-item'>
               <img src={imgTwo} alt='' />
             </div>
             <div className='image-item'>
               <img src={imgThree} alt='' />
+            </div>
+          </div>
+          <div className='description-wrapper'>
+            <div className='content-description'>
+              <h1>PARA TODOS OS GOSTOS !</h1>
+              <p>
+                Seja vegano ou hamburguer artesanal, você irá se apaixonar pelo
+                sabor!
+              </p>
+            </div>
+            <div className='content-description'>
+              <h1>PARA TODOS OS GOSTOS !</h1>
+              <p>
+                Seja vegano ou hamburguer artesanal, você irá se apaixonar pelo
+                sabor!
+              </p>
+            </div>
+            <div className='content-description'>
+              <h1>PARA TODOS OS GOSTOS !</h1>
+              <p>
+                Seja vegano ou hamburguer artesanal, você irá se apaixonar pelo
+                sabor!
+              </p>
             </div>
           </div>
         </header>
