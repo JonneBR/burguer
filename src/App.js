31ead4585bbb2cc1e0decc4e6e87c43fa55ebbb4
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2021-02-09 15:01:26 -0300

    footer component css basic style

--- src/App.js
+++ src/App.js
@@ -4,17 +4,17 @@ import Navbar from './components/Navbar/Navbar';
 import LandingPage from './components/LandingPage/LandingPage';
 import ApresentationPage from './components/ApresentationPage/ApresentationPage';
 import Home from './components/pages/Home';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
-   <Router>
-     <Navbar />
-   <Switch>
-   <Route path="/" exact component={Home} />
-     {/* <LandingPage/>
-     <ApresentationPage/> */}
-   </Switch>
-   </Router>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path='/' exact component={Home} />
+      </Switch>
+      <Footer />
+    </Router>
   );
 }
 
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <>
+      <footer className='footer-container'>
+        <div className='footer-wrapper'></div>
+        <div className='creator-info'></div>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
