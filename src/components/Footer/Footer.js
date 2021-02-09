e7301cfce4b9058002d1f12426e6490e47faab04
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2021-02-09 18:44:52 -0300

    footer mobile friendly css styled

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,21 @@ function Footer() {
   return (
     <>
       <footer className='footer-container'>
-        <div className='footer-wrapper'></div>
+        <div className='footer-wrapper'>
+          <div className='address-info'>
+            <h1>Endereço</h1>
+            <p>
+              Avenida <span>Rodolpho Sales</span> Rua{' '}
+              <span>Flavio Beta, 513</span>
+            </p>
+          </div>
+          <div className='social-medias'>
+            <h1>Fale Conosco</h1>
+            <i class='fab fa-facebook-square'></i>
+            <i class='fab fa-instagram'></i>
+            <i class='fab fa-whatsapp'></i>
+          </div>
+        </div>
         <div className='creator-info'></div>
       </footer>
     </>
