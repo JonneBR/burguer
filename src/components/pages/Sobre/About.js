79d6c12b0da3e46247870c7577b5039b96ab1710
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2021-02-10 14:14:43 -0300

    about div css style

--- src/components/pages/Sobre/About.js
+++ src/components/pages/Sobre/About.js
@@ -5,9 +5,23 @@ function About() {
   return (
     <>
       <div className='about-hero-section'>
-        <h1>teste</h1>
-        <h1>teste</h1>
-        <h1>teste</h1>
+        <div className='container-info-wrapper'>
+          <div className='container-info'>
+            <h1>CONHEÇA A NOSSA HISTÓRIA:</h1>
+            <p>
+              Lorem Ipsum é simplesmente uma simulação de texto da indústria
+              tipográfica e de impressos, e vem sendo utilizado desde o século
+              XVI, quando um impressor desconhecido pegou uma bandeja de tipos e
+              os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum
+              sobreviveu não só a cinco séculos, como também ao salto para a
+              editoração eletrônica, permanecendo essencialmente inalterado. Se
+              popularizou na década de 60, quando a Letraset lançou decalques
+              contendo passagens de Lorem Ipsum, e mais recentemente quando
+              passou a ser integrado a softwares de editoração eletrônica como
+              Aldus PageMaker.
+            </p>
+          </div>
+        </div>
       </div>
     </>
   );
