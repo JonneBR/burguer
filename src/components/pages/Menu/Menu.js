6ab8d677844bdce34091080fa988a167eee47e58
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2021-02-11 18:52:06 -0300

    buttom menu page css style

--- src/components/pages/Menu/Menu.js
+++ src/components/pages/Menu/Menu.js
@@ -14,16 +14,125 @@ function Menu() {
             <div className='burguer-image'>
               <img src={burguerOne} alt='' />
             </div>
-          </div>
-          <div className='item-container'>
-            <div className='burguer-image'>
-              <img src={burguerOne} alt='' />
+            <div className='burguer-name'>
+              <h1>GAUCHO</h1>
+            </div>
+            <div className='burguer-price'>
+              <p>R$35,00</p>
+            </div>
+            <div className='burguer-ingredientes'>
+              <p>
+                Burguer artesanal, queijo prato, presunto, alface, tomate,
+                picles, maionese, ketchup
+              </p>
             </div>
           </div>
           <div className='item-container'>
             <div className='burguer-image'>
               <img src={burguerOne} alt='' />
             </div>
+            <div className='burguer-name'>
+              <h1>GAUCHO</h1>
+            </div>
+            <div className='burguer-price'>
+              <p>R$35,00</p>
+            </div>
+            <div className='burguer-ingredientes'>
+              <p>
+                Burguer artesanal, queijo prato, presunto, alface, tomate,
+                picles, maionese, ketchup
+              </p>
+            </div>
+          </div>
+          <div className='item-container'>
+            <div className='burguer-image'>
+              <img src={burguerOne} alt='' />
+            </div>
+            <div className='burguer-name'>
+              <h1>GAUCHO</h1>
+            </div>
+            <div className='burguer-price'>
+              <p>R$35,00</p>
+            </div>
+            <div className='burguer-ingredientes'>
+              <p>
+                Burguer artesanal, queijo prato, presunto, alface, tomate,
+                picles, maionese, ketchup
+              </p>
+            </div>
+          </div>
+          <div className='item-container'>
+            <div className='burguer-image'>
+              <img src={burguerOne} alt='' />
+            </div>
+            <div className='burguer-name'>
+              <h1>GAUCHO</h1>
+            </div>
+            <div className='burguer-price'>
+              <p>R$35,00</p>
+            </div>
+            <div className='burguer-ingredientes'>
+              <p>
+                Burguer artesanal, queijo prato, presunto, alface, tomate,
+                picles, maionese, ketchup
+              </p>
+            </div>
+          </div>
+          <div className='item-container'>
+            <div className='burguer-image'>
+              <img src={burguerOne} alt='' />
+            </div>
+            <div className='burguer-name'>
+              <h1>GAUCHO</h1>
+            </div>
+            <div className='burguer-price'>
+              <p>R$35,00</p>
+            </div>
+            <div className='burguer-ingredientes'>
+              <p>
+                Burguer artesanal, queijo prato, presunto, alface, tomate,
+                picles, maionese, ketchup
+              </p>
+            </div>
+          </div>
+          <div className='item-container'>
+            <div className='burguer-image'>
+              <img src={burguerOne} alt='' />
+            </div>
+            <div className='burguer-name'>
+              <h1>GAUCHO</h1>
+            </div>
+            <div className='burguer-price'>
+              <p>R$35,00</p>
+            </div>
+            <div className='burguer-ingredientes'>
+              <p>
+                Burguer artesanal, queijo prato, presunto, alface, tomate,
+                picles, maionese, ketchup
+              </p>
+            </div>
+          </div>
+          <div className='item-container'>
+            <div className='burguer-image'>
+              <img src={burguerOne} alt='' />
+            </div>
+            <div className='burguer-name'>
+              <h1>GAUCHO</h1>
+            </div>
+            <div className='burguer-price'>
+              <p>R$35,00</p>
+            </div>
+            <div className='burguer-ingredientes'>
+              <p>
+                Burguer artesanal, queijo prato, presunto, alface, tomate,
+                picles, maionese, ketchup
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className='bottom-container'>
+          <div className='button-container'>
+            <button className='button-subject'>PEÇA AGORA</button>
           </div>
         </div>
       </div>
