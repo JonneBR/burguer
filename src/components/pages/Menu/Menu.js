138eb6e9c9ef5342b5159379e76cf09b790be393
João Marcos Gonçalves <joaomgoncalvesbr@gmail.com>
2021-02-11 17:46:33 -0300

    menu grid images css style

--- src/components/pages/Menu/Menu.js
+++ src/components/pages/Menu/Menu.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Menu.css';
+import burguerOne from '../../../images/burguer-one.jpg';
 
 function Menu() {
   return (
@@ -7,6 +8,23 @@ function Menu() {
       <div className='menu-section'>
         <div className='menu-header-title'>
           <h1>Menu</h1>
+        </div>
+        <div className='menu-list-items'>
+          <div className='item-container'>
+            <div className='burguer-image'>
+              <img src={burguerOne} alt='' />
+            </div>
+          </div>
+          <div className='item-container'>
+            <div className='burguer-image'>
+              <img src={burguerOne} alt='' />
+            </div>
+          </div>
+          <div className='item-container'>
+            <div className='burguer-image'>
+              <img src={burguerOne} alt='' />
+            </div>
+          </div>
         </div>
       </div>
     </>
